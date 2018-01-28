8e83f8a9f16f5409510f3ce9709a869268318f8b
Werner Turing <werner.turing@protonmail.com>
2018-01-28 13:04:43 -0200

    Sample program loading a frame with opencv and displaying on a gtkmm window.

--- gtkshowframe.cpp
+++ gtkshowframe.cpp
@@ -1,19 +1,45 @@
+// g++ -o gtkshowframe `pkg-config gtkmm-3.0 opencv --cflags --libs` gtkshowframe.cpp
+
 #include <gtkmm.h>
+
+#include <opencv2/opencv.hpp>
 
 
 int main(int argc, char *argv[])
 {
   Gtk::Main gtk(argc, argv);
 
-  if (argc != 2) {
+  if (argc != 3) {
     printf("usage: gtkshowframe <Video_Path> <Frame_Number>\n");
     return -1;
   }
 
+  cv::VideoCapture video(argv[1]);
+  if (!video.isOpened()) {
+    printf("Cannot open the video file\n");
+    return -1;
+  }
+
+  int frameNumber = atoi(argv[2]);
+  video.set(CV_CAP_PROP_POS_FRAMES, frameNumber);
+
+  cv::Mat frame;
+  bool success = video.read(frame);
+  if (!success) {
+    printf("Could not read frame\n");
+    return -1;
+  }
+
+  cv::cvtColor(frame, frame, CV_BGR2RGB);
+  auto pixbuf = Gdk::Pixbuf::create_from_data(frame.data,
+                                              Gdk::COLORSPACE_RGB,
+                                              false, 8,
+                                              frame.cols, frame.rows,
+                                              frame.step);
+
   Gtk::Window window;
   window.set_default_size(600, 600);
 
-  auto pixbuf = Gdk::Pixbuf::create_from_file(argv[1]);
   Gtk::Image image(pixbuf);
   window.add(image);
 
