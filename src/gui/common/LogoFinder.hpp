a57a57a4a1b2213cc405180027b80cae9fbeb564
Werner Turing <werner.turing@protonmail.com>
2018-03-20 11:35:00 -0300

    Added functions to change the size of logos to detect.

--- src/gui/common/LogoFinder.hpp
+++ src/gui/common/LogoFinder.hpp
@@ -45,7 +45,9 @@ namespace mdl {
   public:
     LogoFinder(LogoFinderCallback& callback)
       : callback_(callback) { };
+
     virtual ~LogoFinder() { };
+
 
     void set_start_frame(int start_frame) {
       start_frame_ = start_frame;
@@ -58,6 +60,40 @@ namespace mdl {
     void set_extra_frames(int extra_frames) {
       extra_frames_ = extra_frames;
     }
+
+
+    int get_min_logo_width() const {
+      return min_logo_width_;
+    }
+
+    void set_min_logo_width(int min_logo_width) {
+      min_logo_width_ = min_logo_width;
+    }
+
+    int get_max_logo_width() const {
+      return max_logo_width_;
+    }
+
+    void set_max_logo_width(int max_logo_width) {
+      max_logo_width_ = max_logo_width;
+    }
+
+    int get_min_logo_height() const {
+      return min_logo_height_;
+    }
+
+    void set_min_logo_height(int min_logo_height) {
+      min_logo_height_ = min_logo_height;
+    }
+
+    int get_max_logo_height() const {
+      return max_logo_height_;
+    }
+
+    void set_max_logo_height(int max_logo_height) {
+      max_logo_height_ = max_logo_height;
+    }
+
 
     virtual void find_logos() = 0;
 
